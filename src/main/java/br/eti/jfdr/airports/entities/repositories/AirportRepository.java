8d100ef3a8638a1531d2c10dd3b90ec6619d1920
jubs-git <juliafernandes.reis15@gmail.com>
2025-11-27 08:17:01 -0300

    Mensagem de commit V 1.0

---
+++ src/main/java/br/eti/jfdr/airports/entities/repositories/AirportRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.eti.jfdr.airports.entities.repositories;
+
+import br.eti.jfdr.airports.entities.Airport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author DIT2A
+ */
+public interface AirportRepository extends JpaRepository<Airport, Long> {
+    
+}
