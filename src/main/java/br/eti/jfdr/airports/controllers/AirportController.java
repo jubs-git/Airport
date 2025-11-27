b636e835da105d6f13ac774dfbcf274a59a0a017
jubs-git <juliafernandes.reis15@gmail.com>
2025-11-27 10:11:05 -0300

    Término da Fase 3 – Implementado endpoint FindByCityIgnoreCase. - Update AirportRepository – Adicionado assinatura de método findByCityIgnoreCase; - Update AirportService – Adicionado método findByCity - Update AirportController – Adicionado endpoint /city/{cityName}

--- src/main/java/br/eti/jfdr/airports/controllers/AirportController.java
+++ src/main/java/br/eti/jfdr/airports/controllers/AirportController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.eti.jfdr.airports.services.AirportService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  *
@@ -27,4 +29,14 @@ public class AirportController {
         return result;
     }
     
+    @GetMapping("/city/{cityName}")
+    public ResponseEntity<List<Airport>> findByCityIgnoreCase(@PathVariable String cityName) {
+        List<Airport> result = airportService.findByCity(cityName);
+        
+        if (result.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(result);
+        }
+    }
 }
--- src/main/java/br/eti/jfdr/airports/entities/repositories/AirportRepository.java
+++ src/main/java/br/eti/jfdr/airports/entities/repositories/AirportRepository.java
@@ -5,6 +5,7 @@
 package br.eti.jfdr.airports.entities.repositories;
 
 import br.eti.jfdr.airports.entities.Airport;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -12,5 +13,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author DIT2A
  */
 public interface AirportRepository extends JpaRepository<Airport, Long> {
+    
+    List<Airport> findByCityIgnoreCase(String city);
     
 }
